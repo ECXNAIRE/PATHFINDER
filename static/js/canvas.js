597efab28085947bb750ea347868e5ec5991e26a
suryansh <suryanshsinghrajput007@gmail.com>
2026-07-07 12:34:23 +0530

    adding tools

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,8 +1,22 @@
 const canvas = document.getElementById("drawCanvas");
 const ctx = canvas.getContext("2d");
+const toolBtn = document.querySelectorAll(".toolBtn")
+let selectedTool = null
 
+
+toolBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        toolBtn.forEach(btn => {
+            btn.classList.remove("selectedTool")
+        })
+
+        button.classList.add("selectedTool")
+
+        selectedTool = button.dataset.tool
+        console.log(selectedTool)
+    })
+})
 const canvasArea = document.getElementById("canvasArea");
-
 const cellSize = 25;
 
 canvas.width = Math.floor(canvasArea.clientWidth / cellSize) * cellSize;
@@ -10,6 +24,8 @@ canvas.height = Math.floor(canvasArea.clientHeight / cellSize) * cellSize;
 
 const ROWS = canvas.height / cellSize;
 const COLUMNS = canvas.width / cellSize;
+
+console.log(ROWS, COLUMNS)
 
 
 
@@ -71,3 +87,28 @@ function drawCell(cell) {
 }
 
 drawGrid()
+
+
+
+canvas.addEventListener("click", (event) => {
+    const rect = canvas.getBoundingClientRect();
+
+    const mouseX = event.clientX - rect.left
+    const mouseY = event.clientY - rect.top;
+
+    const column = Math.floor(mouseX/ cellSize)
+    const row = Math.floor(mouseY / cellSize)
+
+    console.log(row, column)
+
+    grid[row][column].type = selectedTool
+
+    if(selectedTool === "eraser") {
+        grid[row][column].type = "empty"
+    } else {
+        grid[row][column].type = selectedTool
+    }
+
+    drawGrid()
+})
+
